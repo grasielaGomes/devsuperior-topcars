c01e0fb46daa71cfbd7b70f70d3d87ecbf0c4eaa
Grasiela Gomes <produtoradama@gmail.com>
2021-09-01 20:48:35 -0300

    styling product card mobile

--- src/pages/Catalog/catalog.tsx
+++ src/pages/Catalog/catalog.tsx
@@ -1,10 +1,13 @@
 import { Card } from "components/Card/card";
+import { Product } from "components/Product/product";
 import { Search } from "components/Search/search";
 
 export function Catalog() {
   return (
     <main className="m-2 m-md-4">
       <Card children={<Search />} color={"bg-light"} hasShadow={false} />
+      <Card children={<Product />} color={"bg-light"} hasShadow={false} />
+      <Card children={<Product />} color={"bg-light"} hasShadow={false} />
     </main>
   );
 }
