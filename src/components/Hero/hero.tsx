6479bff93aec1900a3838986c4f11a3c4e63c107
Grasiela Gomes <produtoradama@gmail.com>
2021-09-02 10:07:39 -0300

    styling hero sm

--- src/components/Hero/hero.tsx
+++ src/components/Hero/hero.tsx
@@ -6,12 +6,12 @@ export function Hero(){
       <img
         src={heroImg}
         alt="Carro mais top"
-        className="col-10 col-md-6 my-4"
+        className="col-10 col-sm-8 my-4"
       />
-      <p className="text-light fs-3 fw-bold">O carro perfeito para você</p>
-      <p className="text-light px-2">
+      <h3 className="text-light fw-bold">O carro perfeito para você</h3>
+      <h6 className="text-light px-2 px-sm-5">
         Conheça nossos carros e dê mais um passo na realização do seu sonho
-      </p>
+      </h6>
     </div>
   );
 }
