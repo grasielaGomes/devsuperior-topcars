d414c192815a8ecb070f0bf9cde7d0bef68a2973
Grasiela Gomes <produtoradama@gmail.com>
2021-09-03 09:34:53 -0300

    catalog responsive xl

--- src/pages/Catalog/catalog.tsx
+++ src/pages/Catalog/catalog.tsx
@@ -4,24 +4,58 @@ import { Search } from "components/Search/search";
 
 export function Catalog() {
   return (
-    <main className="m-2 m-sm-4 d-grid gap-2 gap-sm-4">
-      <Card children={<Search />} color={"bg-light"} hasShadow={false} />
-      <section>
-        <div className="row">
-          <div className="col-sm-6 col-lg-4 mb-2 mb-sm-4">
-            <Card children={<Product />} color={"bg-light"} hasShadow={false} />
+    <main className="container-xl">
+      <div className="mt-2 mt-sm-4 mt-md-0 p-md-4 d-grid gap-2 gap-sm-4 col-xl-10 mx-xl-auto">
+        <header>
+          <Card children={<Search />} color={"bg-light"} hasShadow={false} />
+        </header>
+        <section>
+          <div className="row">
+            <div className="col-sm-6 col-lg-4 mb-2 mb-sm-4">
+              <Card
+                children={<Product />}
+                color={"bg-light"}
+                hasShadow={false}
+              />
+            </div>
+            <div className="col-sm-6 col-lg-4 mb-2 mb-sm-4">
+              <Card
+                children={<Product />}
+                color={"bg-light"}
+                hasShadow={false}
+              />
+            </div>
+            <div className="col-sm-6 col-lg-4 mb-2 mb-sm-4">
+              <Card
+                children={<Product />}
+                color={"bg-light"}
+                hasShadow={false}
+              />
+            </div>
+            <div className="col-sm-6 col-lg-4 mb-2 mb-sm-4">
+              <Card
+                children={<Product />}
+                color={"bg-light"}
+                hasShadow={false}
+              />
+            </div>
+            <div className="col-sm-6 col-lg-4 mb-2 mb-sm-4">
+              <Card
+                children={<Product />}
+                color={"bg-light"}
+                hasShadow={false}
+              />
+            </div>
+            <div className="col-sm-6 col-lg-4 mb-2 mb-sm-4">
+              <Card
+                children={<Product />}
+                color={"bg-light"}
+                hasShadow={false}
+              />
+            </div>
           </div>
-          <div className="col-sm-6 col-lg-4 mb-2 mb-sm-4">
-            <Card children={<Product />} color={"bg-light"} hasShadow={false} />
-          </div>
-          <div className="col-sm-6 col-lg-4 mb-2 mb-sm-4">
-            <Card children={<Product />} color={"bg-light"} hasShadow={false} />
-          </div>
-          <div className="col-sm-6 col-lg-4 mb-2 mb-sm-4">
-            <Card children={<Product />} color={"bg-light"} hasShadow={false} />
-          </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </main>
   );
 }
